edc2a2c1f3d50e9e2e2ec359b55da5aeeffc66f3
Adeilson Lima <asl@ic.ufal.br>
2017-03-30 17:53:50 -0300

    Preferencia de cor de fundo

---
+++ PreferenciaCorUsuario/app/src/main/java/br/com/adeilson/preferenciacorusuario/MainActivity.java
@@ -0,0 +1,69 @@
+package br.com.adeilson.preferenciacorusuario;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioSelecionado;
+    private Button botaoSalvar;
+    private RelativeLayout layout;
+    private static final String ARQUIVO_PREFERENCIA = "ArqPreferencia";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupId);
+        botaoSalvar = (Button) findViewById(R.id.botaoSalvarId);
+        layout = (RelativeLayout) findViewById(R.id.layoutId);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int idRadioButtonSelecionado = radioGroup.getCheckedRadioButtonId();
+
+                if(idRadioButtonSelecionado>0){
+                    radioSelecionado = (RadioButton) findViewById(idRadioButtonSelecionado);
+
+                    SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);//modo privado
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    String corSelecionada = radioSelecionado.getText().toString();
+                    editor.putString("corEscolhida", corSelecionada);
+                    editor.commit();
+
+                    setBackground(corSelecionada);
+
+
+                }
+            }
+        });
+
+        //recupera a cor salva
+        SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
+        if (sharedPreferences.contains("corEscolhida")){
+            String corEscolhida = sharedPreferences.getString("corEscolhida","Laranja");
+            setBackground(corEscolhida);
+        }
+    }
+
+
+    private void setBackground(String cor){
+        if(cor.equals("Azul")){
+            layout.setBackgroundColor(Color.parseColor("#495b7c"));
+        }else if(cor.equals("Laranja")){
+            layout.setBackgroundColor(Color.parseColor("#ffce26"));
+        }else if(cor.equals(("Verde"))){
+            layout.setBackgroundColor(Color.parseColor("#009670"));
+        }
+    }
+}
